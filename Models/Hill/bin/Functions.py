5fc91e2655627ab2a8415c66d2738fed682274ff
jodarie <jodarie@users.noreply.github.com>
2020-01-15 17:10:39 -0300

    Update Functions.py

--- Models/Hill/bin/Functions.py
+++ Models/Hill/bin/Functions.py
@@ -70,7 +70,7 @@ def Best_fit(Pmatrix,Xmatrix,DHmatrix,Vc,vi,So,bo,dim,nw,nc,lab,eArgs):
         theta_th[i][j] = theta_func(Xtot,n,K,Ptot[i][j]) 
     if ~np.isnan(np.sum(theta_th)):
       for i in range(nconc):
-        DH_th[:,i] = (DH/CS[i])*((Ptot[1:,i]*theta_th[1:,i]*(0.5+(Vc/vi[1:,i])))+(Ptot[:-1,i]*theta_th[:-1,i]*(0.5-(Vc/vi[:-1,i]))))
+        DH_th[:,i] = (DH/CS[i])*((Ptot[1:,i]*theta_th[1:,i]*(0.5+(Vc[i]/vi[1:,i])))+(Ptot[:-1,i]*theta_th[:-1,i]*(0.5-(Vc[i]/vi[:-1,i]))))
         if eArgs[0]:
           NProb = NProb+(Ndata)*np.log(s[i])+(1.0/(2.0*s[i]*s[i]))*np.sum((DH_th[:,i]-DH_exp[:,i]+q0[i])**2)-ln_Gauss(CS[i],So[i],errSo*So[i])-ln_Gauss(eps[i],eArgs[1][i],eArgs[2][i])
         else:
@@ -176,7 +176,7 @@ def Figure_fit(P,out,Pmatrix,Xmatrix,DHmatrix,Vc,vi,So,color_code):
     for j,Xtot in enumerate(Xrow):
       theta_th[i][j] = theta_func(Xtot,n,K,Ptot[i][j])
   for i in range(nconc):
-    DH_th[:,i] = (DH/CS[i])*((Ptot[1:,i]*theta_th[1:,i]*(0.5+(Vc/vi[1:,i])))+(Ptot[:-1,i]*theta_th[:-1,i]*(0.5-(Vc/vi[:-1,i]))))+q0[i]
+    DH_th[:,i] = (DH/CS[i])*((Ptot[1:,i]*theta_th[1:,i]*(0.5+(Vc[i]/vi[1:,i])))+(Ptot[:-1,i]*theta_th[:-1,i]*(0.5-(Vc[i]/vi[:-1,i]))))+q0[i]
   plt.rc('font', family='serif',size=24)
   fig = plt.figure(figsize=(10,8))
   for i in range(nconc):
